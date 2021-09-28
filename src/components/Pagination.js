a5007219988f5919184f5a3abc697a9cb6142484
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-09-28 22:33:07 +0900

    newsページ追加

---
+++ src/components/Pagination.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export const Pagination = ({ totalCount }) => {
+  const PER_PAGE = 3;
+
+  const range = (start, end) => {
+    return [...Array(end - start + 1)].map((_, i) => {
+      return start + i;
+    });
+  };
+
+  return (
+    <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+      <nav className="flex justify-center">
+        <ul className="flex flex-row">
+          {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => {
+            return (
+              <li key={index}>
+                <Link href={`/news/page/${number}`}>
+                  <a className="mr-6 p-3 text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 focus:from-purple-600 focus:to-yellow-600 opacity-80 rounded">
+                    {number}
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </div>
+  );
+};
---
+++ src/pages/news/[id].js
@@ -0,0 +1,50 @@
+/* eslint-disable import/no-default-export */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+// pages/news/[id].js
+import Link from "next/link";
+// import Date from 'src/lib/date'
+import { client } from "src/lib/client";
+
+export default function BlogId({ news }) {
+  return (
+    <main>
+      <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl mt-6 lg:text-center">{news.title}</h1>
+      <p className="text-right mt-3 mb-10">{news.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          __html: `${news.body}`,
+        }}
+      />
+      <div className="absolute mt-5 text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 opacity-80 p-3 rounded inline-block right-3">
+        <Link href={`/news`}>
+          <a className="">News Top</a>
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "news" });
+
+  const paths = data.contents.map((content) => {
+    return `/news/${content.id}`;
+  });
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "news", contentId: id });
+
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
