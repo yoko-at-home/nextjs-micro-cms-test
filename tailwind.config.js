ad82834667e76fd3df60918efef50873f132e3c8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-09-28 10:09:50 +0900

    tailwindの設定を追加

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-image": "url('/static/images/laurel.png')",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")],
+};
