5bbdf17cdedaf73c414086a44eb8bdfcf58c0043
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-09-28 10:16:07 +0900

    footerを更新

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,23 @@
 import type { VFC } from "react";
+import { siteMetadata } from "src/data/siteMetadata";
+
+import { CustomLink } from "./Link";
 
 export const Footer: VFC = () => {
   return (
     <footer>
-      <small lang="en">@ 20xx example.com</small>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 ">
+          {/* <div>{siteMetadata.author}</div>
+          <div>{` • `}</div> */}
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <CustomLink href="/">{siteMetadata.title}</CustomLink>
+        </div>
+        <div className="my-8 text-sm text-gray-500 ">
+          <CustomLink href="https://over40webclub.netlify.app/">Powered by Over 40 Web Club</CustomLink>
+        </div>
+      </div>
     </footer>
   );
 };
---
+++ src/data/siteMetadata.js
@@ -0,0 +1,25 @@
+export const siteMetadata = {
+  title: "Gen-Scent Research Laboratory",
+  author: "Minako Hada",
+  headerTitle: "Gen-Scent Research Laboratory",
+  description: "Gen-Scent Research Laboratory is a ...",
+  language: "jp-ja",
+  siteUrl: "https://www.gen-scent.com",
+  siteRepo: "https://www.gen-scent.com",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/avatar.png",
+  socialBanner: "/static/images/twitter-card.png",
+  email: "tech＠gen-scent.com",
+  github: "https://github.com",
+  twitter: "https://twitter.com/Twitter",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com",
+  linkedin: "https://www.linkedin.com",
+  locale: "ja-JP",
+  analytics: {
+    // supports plausible, simpleAnalytics or googleAnalytics
+    plausibleDataDomain: "", // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    googleAnalyticsId: "", // e.g. UA-000000-2 or G-XXXXXXX
+  },
+};
