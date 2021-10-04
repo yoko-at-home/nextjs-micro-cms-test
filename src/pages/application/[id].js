bf13ee5a1f20c4b1abf8d4d6a9069aa60eaf388c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-04 19:43:08 +0900

    news [id].jsをコピー

---
+++ src/pages/application/[id].js
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-default-export */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+// pages/news/[id].js
+import Link from "next/link";
+import { LayoutHNewsArticles } from "src/components/layout";
+import { client } from "src/lib/client";
+import { Date } from "src/lib/date";
+
+export default function BlogId({ news }) {
+  const publishedAt = news.publishedAt;
+  const revisedAt = news.revisedAt;
+  return (
+    <LayoutHNewsArticles>
+      <main>
+        <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl mt-6 lg:text-center py-12">{news.title}</h1>
+        <div className="flex flex-col text-right mt-3 mb-10">
+          {publishedAt === revisedAt ? (
+            <div>
+              Published: <Date className="text-sm text-blueGray-500 mb-3" dateString={news.publishedAt} />
+            </div>
+          ) : (
+            <>
+              <div>
+                Revised at: <Date className="text-sm text-blueGray-500" dateString={news.revisedAt} />
+              </div>
+              <div>
+                Published at: <Date className="text-sm text-blueGray-500 mb-3" dateString={news.publishedAt} />
+              </div>
+            </>
+          )}
+        </div>
+        <div
+          dangerouslySetInnerHTML={{
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            __html: `${news.body}`,
+          }}
+        />
+        <div className="absolute mt-5 text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 opacity-80 p-3 rounded inline-block right-10 md:right-20 lg:right-40">
+          <Link href={`/news`}>
+            <a className="">News Top</a>
+          </Link>
+        </div>
+      </main>
+    </LayoutHNewsArticles>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "news" });
+
+  const paths = data.contents.map((content) => {
+    return `/news/${content.id}`;
+  });
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "news", contentId: id });
+
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
