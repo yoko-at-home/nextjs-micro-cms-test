3f0c1372511351ed8e20032f5a52012713eecf40
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-06 19:50:58 +0900

    validationでautocomplete入れる

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -52,6 +52,7 @@ const Contact: NextPage = () => {
                   type="text"
                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   placeholder="お名前"
+                  autoComplete="name"
                   required
                   minLength={3}
                 />
@@ -64,6 +65,7 @@ const Contact: NextPage = () => {
                   type="email"
                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   placeholder="送信可能なメールアドレス形式：name@example.com"
+                  autoComplete="email"
                   required
                 />
               </div>
