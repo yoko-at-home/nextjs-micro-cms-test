c365b537cc6dd8f70774eaf6960c57f6bc20e77c
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-07 13:12:03 +0900

    文言修正

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,8 +9,8 @@ export const Footer: VFC = () => {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 ">
-          {/* <div>{siteMetadata.author}</div>
-          <div>{` • `}</div> */}
+          <div>代表: {siteMetadata.author}</div>
+          <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <CustomLink href="/">{siteMetadata.title}</CustomLink>
