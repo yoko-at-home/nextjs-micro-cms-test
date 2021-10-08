97ae08588e3ec0d8578724b741e4cf83a19b7487
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-08 11:25:39 +0900

    microCMSブログからコードをコピー

---
+++ src/pages/news/[slug].js
@@ -0,0 +1,15 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable @typescript-eslint/naming-convention */
+export const getStaticProps = async (context) => {
+  const slug = context.params?.slug;
+  const draftKey = context.previewData?.draftKey;
+  const content = await fetch(
+    `https://xxxxxx.microcms.io/api/v1/blog/${slug}${draftKey !== undefined ? `?draftKey=${draftKey}` : ""}`,
+    { headers: { "X-API-KEY": process.env.apiKey || "" } }
+  ).then((res) => res.json());
+  return {
+    props: {
+      content,
+    },
+  };
+};
