3a590fa057c1bcb5d20249316a15a9dccb072d62
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-11 16:25:32 +0900

    ログアウト時の表示（仮）を実装

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -15,24 +15,31 @@ const Members: NextPage = () => {
 
       <PageTitle>Members</PageTitle>
       <div className="divide-y divide-gray-200 ">
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <p className="text-lg leading-7 text-gray-500 ">会員登録ページです。</p>
-        </div>
         <div className="container py-12">
           {!user ? (
-            <Link href="/api/auth/login">
-              <a className="mt-12">Login</a>
-            </Link>
-          ) : (
-            <div className="flex justify-between w-48">
-              <Link href="/api/auth/logout">
-                <a className="mt-12">Logout</a>
-              </Link>
-
-              <Link href="/members/profile">
-                <a className="mt-12">プロフィール</a>
+            <div>
+              <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+                <p className="text-lg leading-7 text-gray-500 ">
+                  Webサイトのすべての商品情報にアクセスするには、会員登録をお願いしております。以下のログインリンクからメールアドレスを認証して、会員登録の申請をお願いします。
+                </p>
+              </div>
+              <Link href="/api/auth/login">
+                <a className="mb-5 p-5 bg-gray-300">Login</a>
               </Link>
             </div>
+          ) : (
+            <>
+              <div className="flex justify-between">
+                <Link href="/api/auth/logout">
+                  <a className="p-5 bg-gray-300">Logout</a>
+                </Link>
+
+                <Link href="/members/profile">
+                  <a className="p-5 bg-gray-300">プロフィール</a>
+                </Link>
+              </div>
+              メンバー登録のコンポーネントをここにインポート
+            </>
           )}
         </div>
       </div>
