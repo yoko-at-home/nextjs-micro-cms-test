a56dc270b27c37bdb406868689b1090797926d95
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-11 16:01:35 +0900

    ログイン処理の実装

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import { useUser } from "@auth0/nextjs-auth0";
 import type { NextPage } from "next";
-// import { Carousel } from "src/components/Carousel";
+import Link from "next/link";
 import { Layout } from "src/components/layout";
 import { PageTitle } from "src/components/PageTitle";
 import { PageSEO } from "src/components/SEO";
 import { siteMetadata } from "src/data/siteMetadata";
 
 const Members: NextPage = () => {
+  const { user } = useUser();
   return (
     <Layout>
       <PageSEO title={`Members- ${siteMetadata.author}`} description={siteMetadata.description} />
@@ -16,7 +18,23 @@ const Members: NextPage = () => {
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <p className="text-lg leading-7 text-gray-500 ">会員登録ページです。</p>
         </div>
-        <div className="container py-12">{/* <Carousel /> */}</div>
+        <div className="container py-12">
+          {!user ? (
+            <Link href="/api/auth/login">
+              <a className="mt-12">Login</a>
+            </Link>
+          ) : (
+            <div className="flex justify-between w-48">
+              <Link href="/api/auth/logout">
+                <a className="mt-12">Logout</a>
+              </Link>
+
+              <Link href="/members/profile">
+                <a className="mt-12">プロフィール</a>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
