c6c48eea54fde3b8a008f30bc8b7ebbcf2eea025
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-14 09:09:29 +0900

    🚧 型定義が未完了

--- src/components/social-icons/index.tsx
+++ src/components/social-icons/index.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
+import type { VFC } from "react";
+
 import Facebook from "./facebook.svg";
 import Github from "./github.svg";
 import Linkedin from "./linkedin.svg";
@@ -17,21 +19,27 @@ const components = {
   twitter: Twitter,
 };
 
-export const SocialIcon = ({ kind, href, size = 8 }) => {
-  if (!href) return null;
+type Props = {
+  kind?: "mail" | "github" | "facebook" | "youtube" | "linkedin" | "twitter";
+  href?: string;
+  size: string;
+};
+// export const SocialIcon: VFC<Props> = ({ kind, href, size = 8 }) => {
+export const SocialIcon: VFC<Props> = (props) => {
+  if (!props.href) return null;
 
-  const SocialSvg = components[kind];
+  const SocialSvg = components[props.kind || "mail"];
 
   return (
     <a
       className="text-sm text-gray-500 transition hover:text-gray-600"
       target="_blank"
       rel="noopener noreferrer"
-      href={href}
+      href={props.href}
     >
-      <span className="sr-only">{kind}</span>
+      <span className="sr-only">{props.kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 h-${props.size} w-${props.size}`}
       />
     </a>
   );
