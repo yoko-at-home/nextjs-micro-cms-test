b37a6df00e54ee6feba153848b9abc02f4dcde7f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-15 08:31:25 +0900

    ログイン後のページ遷移処理を追加

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,4 +1,15 @@
 /* eslint-disable import/no-default-export */
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    await handleLogin(req, res, {
+      returnTo: "/members",
+    });
+  },
+  async logout(req, res) {
+    await handleLogin(req, res, {
+      returnTo: "/members",
+    });
+  },
+});
