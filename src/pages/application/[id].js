55741c35de184de98bedbeefa4ad2b84bd6e016a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-14 09:35:21 +0900

    🚨 lint errorを改善

--- src/pages/application/[id].js
+++ src/pages/application/[id].js
@@ -1,32 +1,31 @@
-/* eslint-disable import/no-default-export */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 // pages/application/[id].js
 import Link from "next/link";
-import { LayoutHNewsArticles } from "src/components/layout";
+import { Layout } from "src/components/layout";
 import { client } from "src/lib/client";
 import { Date } from "src/lib/date";
 
-export default function ApplicationId({ news }) {
-  const publishedAt = news.publishedAt;
-  const revisedAt = news.revisedAt;
+export const ApplicationId = (props) => {
+  const publishedAt = props.news.publishedAt;
+  const revisedAt = props.news.revisedAt;
   return (
-    <LayoutHNewsArticles>
+    <Layout theme="newsArticles">
       <main>
-        <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl mt-6 lg:text-center py-12">{news.title}</h1>
+        <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl mt-6 lg:text-center py-12">
+          {props.news.title}
+        </h1>
         <div className="flex flex-col text-right mt-3 mb-10">
           {publishedAt === revisedAt ? (
             <div>
-              Published: <Date className="text-sm text-blueGray-500 mb-3" dateString={news.publishedAt} />
+              Published: <Date className="text-sm text-blueGray-500 mb-3" dateString={props.news.publishedAt} />
             </div>
           ) : (
             <>
               <div>
-                Revised at: <Date className="text-sm text-blueGray-500" dateString={news.revisedAt} />
+                Revised at: <Date className="text-sm text-blueGray-500" dateString={props.news.revisedAt} />
               </div>
               <div>
-                Published at: <Date className="text-sm text-blueGray-500 mb-3" dateString={news.publishedAt} />
+                Published at: <Date className="text-sm text-blueGray-500 mb-3" dateString={props.news.publishedAt} />
               </div>
             </>
           )}
@@ -34,7 +33,7 @@ export default function ApplicationId({ news }) {
         <div
           dangerouslySetInnerHTML={{
             // eslint-disable-next-line @typescript-eslint/naming-convention
-            __html: `${news.body}`,
+            __html: `${props.news.body}`,
           }}
         />
         <div className="absolute mt-5 text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 opacity-80 p-3 rounded inline-block right-10 md:right-20 lg:right-40">
@@ -43,9 +42,9 @@ export default function ApplicationId({ news }) {
           </Link>
         </div>
       </main>
-    </LayoutHNewsArticles>
+    </Layout>
   );
-}
+};
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
