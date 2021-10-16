a7adb9d5d8595e21303679f3ae6dab5e7adc4a8f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-16 11:13:09 +0900

    useUserのインポート

--- src/pages/application/index.jsx
+++ src/pages/application/index.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 // import type { NextPage } from "next";
 // import type { VFC } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 import { Layout } from "src/components/layout";
 import { PageTitle } from "src/components/PageTitle";
@@ -37,6 +38,19 @@ const AppCard = (props) => {
 
 // const Application: NextPage<ItemType> = () => {
 const Application = (props) => {
+  const { user, error, isLoading } = useUser();
+  if (isLoading)
+    return (
+      <div className=" flex justify-center items-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500">Loading...</div>
+      </div>
+    );
+  if (error)
+    return (
+      <Layout theme="main">
+        <div>{error.message}</div>
+      </Layout>
+    );
   return (
     <Layout theme="main">
       <PageSEO title={`Application- ${siteMetadata.author}`} description={siteMetadata.description} />
