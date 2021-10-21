0bee8b78b4cbe02d478555a6f92edc683dd51629
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-10-21 11:52:11 +0900

    cmsにリッチエディタ使用

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,16 +27,12 @@ const Home = (props) => {
     <Layout theme="main">
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
 
-      <div className="divide-y divide-gray-200 ">
-        <div className="pt-6 md:mt-8 pb-8 space-y-2 md:space-y-5">
+      <div className="divide-y divide-gray-200">
+        <div className="pt-6 md:mt-8 pb-8 space-y-2 md:space-y-5  text-pink-500">
           <PageTitle>{props.content.title}</PageTitle>
-          <PageTitle title={props.content.title} />
-          <p className="text-lg leading-7 text-gray-500 ">
-            {/* {siteMetadata.description}
-            <br /> */}
-            {/* {content.body} */}
-          </p>
+          <p className="text-lg leading-7 text-gray-500 mt-52">{/* {siteMetadata.description} */}</p>
           <div
+            className="text-lg text-pink-500 pt-25"
             dangerouslySetInnerHTML={{
               // eslint-disable-next-line @typescript-eslint/naming-convention
               __html: `${props.content.body}`,
