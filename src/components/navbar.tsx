6313ad73cf43ecb036cb02b130f56297b62dc6d4
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-07 22:42:54 +0900

    🚨 fix lint error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,11 +15,11 @@ const items = [
 
 export const NavBarDesktop: VFC = () => {
   return (
-    <nav className="text-center mt-40 hidden sm:flex justify-center">
+    <nav className="hidden justify-center mt-40 text-center sm:flex">
       {items.map(({ href, label }) => {
         return (
           <Link key={href} href={href}>
-            <a className="p-1 text-center font-medium text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 focus:from-purple-600 focus:to-yellow-600 opacity-80 mr-1 lg:mr-3 lg:text-2xl lg:py-2 hover:text-gray-100 whitespace-nowrap">
+            <a className="p-1 mr-1 font-medium text-center text-gray-300 hover:text-gray-100 whitespace-nowrap bg-gradient-to-r from-gray-400 focus:from-purple-600 to-gray-500 focus:to-yellow-600 opacity-80 sm:px-4 lg:py-2 lg:mr-3 lg:text-2xl">
               {label}
             </a>
           </Link>
@@ -46,10 +46,10 @@ export const NavBarMobile: VFC = () => {
   return (
     <>
       {/* <div className="sm:hidden absolute top-6 left-6"> */}
-      <div className="sm:hidden bg-gray-400 bg-opacity-80 rounded pt-1 px-2 fixed right-5 bottom-10">
+      <div className="fixed right-5 bottom-10 px-2 pt-1 bg-gray-400 bg-opacity-80 rounded sm:hidden">
         <button
           type="button"
-          className="w-8 h-8 ml-1 mr-1 rounded"
+          className="mr-1 ml-1 w-8 h-8 rounded"
           aria-label="Toggle Menu"
           onClick={handleOnToggleNav}
         >
@@ -77,17 +77,17 @@ export const NavBarMobile: VFC = () => {
           <button
             type="button"
             aria-label="toggle modal"
-            className="fixed w-full h-4/5 -top-7 cursor-auto focus:outline-none"
+            className="fixed -top-7 w-full h-4/5 cursor-auto focus:outline-none"
             onClick={handleOnToggleNav}
           ></button>
-          <nav className="text-center mt-40">
+          <nav className="mt-40 text-center">
             {items.map(({ href, label }) => {
               return (
                 <CustomLink
                   key={href}
                   href={href}
                   onClick={handleOnToggleNav}
-                  className="flex flex-col text-2xl tracking-widest p-1 font-medium mx-auto text-gray-300 sm:px-4 bg-gradient-to-r from-gray-400 to-gray-500 focus:from-green-700 focus:to-yellow-500 opacity-90 hover:text-gray-100 text-right pr-3 py-3"
+                  className="flex flex-col p-1 py-3 pr-3 mx-auto text-2xl font-medium tracking-widest text-right text-gray-300 hover:text-gray-100 bg-gradient-to-r from-gray-400 focus:from-green-700 to-gray-500 focus:to-yellow-500 opacity-90 sm:px-4"
                 >
                   {label}
                 </CustomLink>
