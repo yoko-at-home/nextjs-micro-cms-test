87873c247cb1f1d02796e84ad51a84c45c452e0d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-08 17:14:36 +0900

    フォント色の指定を削除して共通化

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,6 +1,6 @@
 export const PageTitle = (props) => {
   return (
-    <h1 className="mt-10 mb-5 lg:mb-10 text-3xl font-extrabold leading-9 tracking-tight text-gray-900  sm:text-4xl sm:leading-10 md:text-5xl md:leading-10 md:mb-16">
+    <h1 className="mt-10 mb-5 lg:mb-10 text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-10 md:mb-16">
       {props.children}
     </h1>
   );
