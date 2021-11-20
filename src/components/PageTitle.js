180d595ea2e570f8e148a205dda0b8470199ce6b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-20 22:56:53 +0900

    💄

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,6 +1,6 @@
 export const PageTitle = (props) => {
   return (
-    <h1 className="mt-10 mb-5 text-3xl font-extrabold tracking-tight leading-9 sm:text-4xl sm:leading-10 md:mb-16 md:text-5xl md:leading-10 lg:mb-10">
+    <h1 className="mt-20 md:mt-32 mb-5 text-3xl font-extrabold tracking-tight leading-9 sm:text-4xl sm:leading-10 md:mb-16 md:text-5xl md:leading-10 lg:mb-10">
       {props.children}
     </h1>
   );
